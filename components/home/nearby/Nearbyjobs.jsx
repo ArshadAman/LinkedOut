9a3a68b07c651cd5181c00b7cf4d4ad2c0d4bd7d
ArshadAman <arshadaman202@gmail.com>
2023-04-19 19:42:40 +0530

    Popular and Nearby Jobs

--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -1,14 +1,42 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ActivityIndicator,
+} from "react-native";
+import { useRouter } from "expo-router";
 
-import styles from './nearbyjobs.style'
+import styles from "./nearbyjobs.style";
+import { COLORS, icons, SIZES } from "../../../constants";
+import useFetch from '../../../hooks/useFetch'
+import NearbyJobCard from "../../common/cards/nearby/NearbyJobCard";
 
 const Nearbyjobs = () => {
-  return (
-    <View>
-      <Text>Nearbyjobs</Text>
-    </View>
-  )
-}
+  const router = useRouter();
 
-export default Nearbyjobs
+  const {data, isLoading, error} = useFetch("search", {query:"React Developer", num_pages: 1});
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Nearby Jobs</Text>
+        <TouchableOpacity style={styles.headerBtn}><Text>Show all</Text></TouchableOpacity>
+      </View>
+
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size="large" color={COLORS.primary} />
+        ):error?(
+          <Text>Something went wrong</Text>
+        ):(
+            data.data?.map((job)=>(
+              <NearbyJobCard job={job} key={job.job_id} handleNavigate={()=>router.push(`job-details/${job_id}`)} />
+            ))
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default Nearbyjobs;
--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -12,19 +12,50 @@ import { useRouter } from "expo-router";
 import styles from "./welcome.style";
 import { icons, SIZES } from "../../../constants";
 
+const jobTypes = ["Full-time", "Part-time", "Intern"];
+
 const Welcome = () => {
   const router = useRouter();
+  const [activeJobType, setactiveJobType] = useState("Full-time");
   return (
     <View style={styles.container}>
       <Text style={styles.userName}>Hello Arshad</Text>
       <Text style={styles.welcomeMessage}>Find Your Perfect Job</Text>
       <View style={styles.searchContainer}>
         <View style={styles.searchWrapper}>
-          <TextInput style={styles.searchInput} value="" onChange={()=>{}} placeholder="Search the jobs" />
+          <TextInput
+            style={styles.searchInput}
+            value=""
+            onChange={() => {}}
+            placeholder="Search the jobs"
+          />
         </View>
-        <TouchableOpacity>
-          <Image/>
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          />
         </TouchableOpacity>
+      </View>
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setactiveJobType(item);
+                router.push(`/search/${item}`);
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={item => item}
+          contentContainerStyle={{columnGap:SIZES.small}}
+          horizontal
+        />
       </View>
     </View>
   );
